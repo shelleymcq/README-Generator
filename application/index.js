1cf4d6aa01c19713f1601e4cf3db61863ee79ff6
Shelley McHardy <shelley.mchardy@gmail.com>
2021-06-23 11:39:52 -0400

    added licenses and comments

--- application/index.js
+++ application/index.js
@@ -6,6 +6,7 @@ const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile)
 
+// prompts for each section of the README
 const getInput = () => {
     return inquirer.prompt(
     // array of questions for user input
@@ -34,7 +35,20 @@ const getInput = () => {
             type: 'list',
             message: 'Choose license.',
             name: 'license',
-            choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser Genergal Public License v2.1', 'Mozilla Public Liscense 2.0', 'The Unlicense']
+            choices: [
+                'Apache License 2.0', 
+                'GNU General Public License v3.0', 
+                'MIT License', 
+                'BSD 2-Clause Simplified License', 
+                'BSD 3-Clause New or Revised License', 
+                'Boost Software License 1.0', 
+                'Creative Commons Zero v1.0 Universal', 
+                'Eclipse Public License 2.0', 
+                'GNU Affero General Public License v3.0', 
+                'GNU General Public License v2.0', 
+                'GNU Lesser Genergal Public License v2.1', 
+                'Mozilla Public Liscense 2.0', 
+                'The Unlicense']
         },
         {
             type: 'input',
@@ -59,13 +73,15 @@ const getInput = () => {
     ]);
 }
 
-
+// function takes user input and add to markdown
 const generateMarkdown = (answers) => 
 
 ` 
 # ${answers.title}
-## License
-${answers.license}
+## Licensing:
+#### _${answers.license}_
+---
+---
 ## Description
 ${answers.description}
 ## Table of Contents
@@ -83,15 +99,16 @@ ${answers.contributors}
 ## Testing
 ${answers.test}
 ## Questions 
-#### To contribute, please contact me by email with any recommended changes.
+#### To contribute, please contact me by email.
 
 github.com/${answers.username} or email me at ${answers.email}
 `;
 
+
 const init = () => {
     getInput()
-    .then((answers) => writeFileAsync('markdown.md', generateMarkdown(answers)))
-    .then(() => console.log('Successfully wrote to markdown.md'))
+    .then((answers) => writeFileAsync('README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully wrote README file'))
     .catch((err) => console.error(err));
 };
 
